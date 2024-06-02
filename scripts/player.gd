17286db3a7edc8c82bf8bd3423e7d7c9f33842b3
Tytus Kalicki <tytuskalicki@Tytuss-MacBook-Pro.local>
2024-06-02 18:56:04 +0200

    ruch

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,9 +3,11 @@ extends RigidBody3D
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_accept"):
-		apply_central_force(Vector3.UP)
+		apply_central_force(basis.y * delta * 1000.0)
 		
 	if Input.is_action_pressed("ui_left"):
-		rotate_z(delta)
+		apply_torque(Vector3(0.0, 0.0, 100.0 * delta))
 	if Input.is_action_pressed("ui_right"):
-		rotate_z(-delta)
+		apply_torque(Vector3(0.0, 0.0, -100.0 * delta))
+		
+	
